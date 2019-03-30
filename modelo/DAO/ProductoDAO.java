60d1625ccf93e70b0728e634178e62c90db3a349
juanjose <juanjose@JU4NG1R4L>
2019-03-30 11:10:29 -0500

    Se realizaron cambios

--- modelo/DAO/ProductoDAO.java
+++ modelo/DAO/ProductoDAO.java
@@ -9,12 +9,16 @@ import producto.ClsProducto;
 /**
  * @author (Juan José Giraldo Salazar)
  *
- * juanj8845@gmail.com
+ *         juanj8845@gmail.com
  */
-public class ProductoDAO extends ClsConexion{
-ClsProducto dao = new ClsProducto();
-public boolean guardarProducto(String nombreProducto, float precioProducto, String descripcionProducto) {
-	return false;
+public class ProductoDAO extends ClsConexion {
+	ClsProducto dao = new ClsProducto();
+
+	public boolean guardarProducto(ClsProducto producto) {
+		String consulta = "insert into producto" + "(nombreProducto, precioProducto, descripcionProducto) " + "values "
+				+ "('" + producto.getNombreProducto() + "', " + producto.getPrecioProducto() + ", '"
+				+ producto.getDescripcionProducto() + ")";
+		return super.ejecutar(consulta);
+	}
 	
-}
 }
