5e279180f531fd1d4fa522ea787c7da9ec8c584e
juanjose <juanjose@JU4NG1R4L>
2019-03-30 22:32:23 -0500

    Se realizaron cambios

--- modelo/DAO/ProductoDAO.java
+++ modelo/DAO/ProductoDAO.java
@@ -3,11 +3,18 @@
  */
 package DAO;
 
+import conexion.ClsConexion;
+import producto.ClsProducto;
+
 /**
  * @author (Juan José Giraldo Salazar)
  *
  * juanj8845@gmail.com
  */
-public class ProductoDAO {
-
+public class ProductoDAO extends ClsConexion{
+ClsProducto dao = new ClsProducto();
+public boolean guardarProducto(String nombreProducto, float precioProducto, String descripcionProducto) {
+	return false;
+	
+}
 }
